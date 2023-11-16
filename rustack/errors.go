e52f1e7fffaeb9e87bf0da61f6c1705ec6bd1cdb
Artem Vessellook <vessellook@gmail.com>
2023-11-16 17:04:43 +0300

    add .ErrorAliases() to RustackApiError (#17)
    Co-authored-by: Васелюк Артём Сергеевич <asvaselyuk@sbcloud.ru>

--- rustack/errors.go
+++ rustack/errors.go
@@ -1,26 +1,36 @@
 package rustack
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
 type RustackApiError struct {
-	msg  string
-	code int
+	msg          string
+	code         int
+	body         []byte
+	errorAliases []string
 }
 
 func NewRustackApiError(url string, resp *http.Response) error {
 	body, _ := io.ReadAll(resp.Body)
 	msg := fmt.Sprintf("HTTP request failure on %s:\n%d: %s", url, resp.StatusCode, string(body))
-
+	var parsedBody struct {
+		ErrorAliases []string `json:"error_alias"`
+	}
+	json.Unmarshal(body, &parsedBody)
 	return &RustackApiError{
-		msg:  msg,
-		code: resp.StatusCode,
+		msg:          msg,
+		code:         resp.StatusCode,
+		body:         body,
+		errorAliases: parsedBody.ErrorAliases,
 	}
 }
 
-func (e *RustackApiError) Error() string   { return e.msg }
-func (e *RustackApiError) Message() string { return e.msg }
-func (e *RustackApiError) Code() int       { return e.code }
+func (e *RustackApiError) Error() string          { return e.msg }
+func (e *RustackApiError) Message() string        { return e.msg }
+func (e *RustackApiError) Code() int              { return e.code }
+func (e *RustackApiError) Body() []byte           { return e.body }
+func (e *RustackApiError) ErrorAliases() []string { return e.errorAliases }
